db4204ee3bce80b2a2e631ec911ea82d73231279
alexandre dias <xanderio@outlook.com>
2024-09-11 21:49:20 -0300

    Adicionar arquivos de projeto.

---
+++ API/Models/produto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float Valor { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
